1751432f418ab1713ab2773fb6506eea79e44749
HarshavardhanPR <165755963+HarshavardhanPR@users.noreply.github.com>
2026-03-24 19:58:51 +0530

    Add testbench for 2x1 multiplexer

---
+++ Examples/mux_2_1_tb.v
@@ -0,0 +1,20 @@
+module tb;
+ reg a, b, sel;
+ wire c;
+ integer i;
+
+ mux_2x1 u0 ( .a(a), .b(b), .sel(sel), .c(c));
+
+ initial begin
+   a <= 0;
+   b <= 0;
+   sel <= 0;
+
+   $monitor("a=%0b b=%0b sel=%0b c=%0b", a, b, sel, c);
+
+   for (i = 0; i < 8; i = i + 1) begin
+     {a, b, sel} = i;
+     #10;
+   end
+ end
+endmodule
