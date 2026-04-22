86f5ac1b66a27cce454f2cb5f026e3940d9d0161
HarshavardhanPR <165755963+HarshavardhanPR@users.noreply.github.com>
2026-04-22 09:50:39 +0530

    Add testbench for even frequency divider module

---
+++ Examples/even_freq_div_tb.v
@@ -0,0 +1,28 @@
+module even_freq_div_tb;
+  reg clk = 0;
+  reg rst;
+  reg t;
+  wire q,q4,q8;
+  
+  even_freq_div DUT(.clk(clk),.rst(rst),.t(t),.q(q),.q4(q4),.q8(q8));
+  
+  always #10 clk = ~clk;
+  
+  initial begin
+    rst=0;t=0;
+    #5;t=1;
+    rst=1; #6;
+    rst=0;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    #200;
+    $finish;
+  end
+  
+endmodule
+  
+  
+  
