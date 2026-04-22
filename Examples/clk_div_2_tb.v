929b8736ebc398864ef1a8f6da4ec66cd0fe2485
HarshavardhanPR <165755963+HarshavardhanPR@users.noreply.github.com>
2026-04-22 10:03:23 +0530

    Add testbench for clk_div_2 module

---
+++ Examples/clk_div_2_tb.v
@@ -0,0 +1,27 @@
+module clk_div_2_tb;
+  reg clk = 0;
+  reg rst;
+  wire q;
+  
+  clk_div_2 DUT(.clk(clk),.rst(rst),.q(q));
+  
+  always #10 clk = ~clk;
+  
+  initial begin
+    rst=0;
+    #5;
+    rst=1; #6;
+    rst=0;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    #200;
+    $finish;
+  end
+  
+endmodule
+  
+  
+  
