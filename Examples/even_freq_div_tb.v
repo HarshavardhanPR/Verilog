410a87a2d74d599895bff07fbabfb60c8fa2e5b4
HarshavardhanPR <165755963+HarshavardhanPR@users.noreply.github.com>
2026-04-22 10:19:54 +0530

    Comment out 2nd clk_div_even_tb module
    Commented out the clk_div_even_tb module for future reference.

--- Examples/even_freq_div_tb.v
+++ Examples/even_freq_div_tb.v
@@ -23,6 +23,32 @@ module even_freq_div_tb;
   end
   
 endmodule
+
+/*
+module clk_div_even_tb;
+  reg clk = 0;
+  reg rst;
+  wire q,q4,q8;
   
+  clk_div_even DUT(.clk(clk),.rst(rst),.q(q),.q4(q4),.q8(q8));
+  
+  always #10 clk = ~clk;
+  
+  initial begin
+    rst=0;
+    #5;
+    rst=1; #6;
+    rst=0;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    #200;
+    $finish;
+  end
+  
+endmodule
+*/
   
   
