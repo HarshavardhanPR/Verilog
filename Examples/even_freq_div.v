a462bab488d044001378654cfa537c85c5b9205c
HarshavardhanPR <165755963+HarshavardhanPR@users.noreply.github.com>
2026-04-22 10:18:58 +0530

    Comment out 2nd clk_div_even module in even_freq_div.v
    Commented out the clk_div_even module code for future reference.

--- Examples/even_freq_div.v
+++ Examples/even_freq_div.v
@@ -10,3 +10,18 @@ end
   always@(posedge q4) q8<=~q8;
     
 endmodule
+
+/*
+module clk_div_even (input clk,rst,output reg q,q4,q8);
+reg [2:0] count;
+// count = 000 => 001 => 010 => 011 => 100 => 101 => 110 => 111
+always@(posedge clk) begin
+  if(rst) count <= 3'b000;
+  else count <= count+1'b1;  
+end    
+  
+assign q = count[0];
+assign q4 = count[1];
+assign q8 = count[2];
+endmodule
+*/
